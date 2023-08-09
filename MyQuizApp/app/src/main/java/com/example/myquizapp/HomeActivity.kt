1f59210ed2017a402c0a6a3b2e0a7ad46c5f8637
DupieM <221326@virtualwindow.co.za>
2023-08-09 23:51:07 +0200

    spellings mistakes fixed

--- MyQuizApp/app/src/main/java/com/example/myquizapp/HomeActivity.kt
+++ MyQuizApp/app/src/main/java/com/example/myquizapp/HomeActivity.kt
@@ -1,8 +1,12 @@
 package com.example.myquizapp
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class HomeActivity : AppCompatActivity() {
-
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
 }
 
